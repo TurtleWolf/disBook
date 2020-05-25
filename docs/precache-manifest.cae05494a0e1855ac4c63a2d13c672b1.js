d46011928526f46ad3e43627def00f9c64a38301
TurtleWolfe <JonPohlner@gmail.com>
2020-05-25 16:10:36 -0400

    no emojis on MyButton.. still sad, but built

---
+++ docs/precache-manifest.cae05494a0e1855ac4c63a2d13c672b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb27208895af6f9aceefbb8db8d4f76",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.19cadfe9ef39ecdbe7ea.bundle.js"
+  },
+  {
+    "url": "runtime~main.19cadfe9ef39ecdbe7ea.bundle.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "url": "vendors~main.19cadfe9ef39ecdbe7ea.bundle.js"
+  },
+  {
+    "url": "vendors~main.19cadfe9ef39ecdbe7ea.bundle.js.LICENSE.txt"
+  }
+]);
