520c0b207be0a67ab1b01ba54d3cfa0996fd47b4
TurtleWolfe <JonPohlner@gmail.com>
2020-05-26 15:58:17 -0400

    my dice is rolling, but no actions showing up at all now?

--- src/components/MyDice/MyDice.stories.js
+++ src/components/MyDice/MyDice.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MyDice from '../MyDice/MyDice';
 import imageUrl from '../../shared/images/logo.svg';
 import { action } from '@storybook/addon-actions';
-import { withCssResources } from '@storybook/addon-cssresources';
 
 export default {
   title: 'MyDice',
@@ -14,15 +13,6 @@ export default {
       'https://www.example.com', // link to a webpage
       'https://www.example.com?id={id}', // link to a webpage with the current story's id in the url
     ],
-    cssresources: [
-      {
-        id: `bluetheme`,
-        code: `<style>body { background-color: lightblue; }</style>`,
-        picked: false,
-        hideCode: false, // Defaults to false, this enables you to hide the code snippet and only displays the style selector
-      },
-    ],
-    decorators: [withCssResources],
   },
 };
 
